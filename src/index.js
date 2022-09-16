1ace0569f1cf475227caee8afa86260172c5ecad
Pedro <pedrobarbosar12@gmail.com>
2022-09-16 15:35:02 -0300

    desenvolvendo o código

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Primeiro from './components/Primeiro'
+import ComParametro from './components/ComParametro'
+
 ReactDOM.render(
-    <h1>Olá React!</h1>,
+    <div>
+        <Primeiro></Primeiro>
+        <ComParametro titulo="Esse é o título"
+        subtitulo="Esse é o subítulo"></ComParametro>
+        <ComParametro titulo="Opa" subtitulo="Epa"></ComParametro>
+    </div>,
     document.getElementById('root')
 )
